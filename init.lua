750d02217d3da495e88b1bd578779b7c82d92578
Lennox <lennox08@tutanota.com>
2024-09-01 10:14:37 +0800

    latex snippets

--- init.lua
+++ init.lua
@@ -241,17 +241,24 @@ require('lazy').setup({
     end,
   },
 
+  --{
+  --  'iurimateus/luasnip-latex-snippets.nvim',
+  --  init = function()
+  --    require('luasnip-latex-snippets').setup()
+  --    -- or setup({ use_treesitter = true })
+  --    require('luasnip').config.setup { enable_autosnippets = true }
+  --  end,
+  --},
+
+  { 'evesdropper/luasnip-latex-snippets.nvim' },
+
   {
-    'L3MON4D3/LuaSnip',
+    'KeitaNakamura/tex-conceal.vim',
     init = function()
       vim.cmd [[
-        " Use Tab to expand and jump through snippets
-        imap <silent><expr> <Tab> luasnip#expand_or_jumpable() ? '<Plug>luasnip-expand-or-jump' : '<Tab>' 
-        smap <silent><expr> <Tab> luasnip#jumpable(1) ? '<Plug>luasnip-jump-next' : '<Tab>'
-
-        " Use Shift-Tab to jump backwards through snippets
-        imap <silent><expr> <S-Tab> luasnip#jumpable(-1) ? '<Plug>luasnip-jump-prev' : '<S-Tab>'
-        smap <silent><expr> <S-Tab> luasnip#jumpable(-1) ? '<Plug>luasnip-jump-prev' : '<S-Tab>'
+        set conceallevel=1
+        let g:tex_conceal='abdmg'
+        hi Conceal ctermbg=none
       ]]
     end,
   },
@@ -938,5 +945,23 @@ require('lazy').setup({
 vim.cmd.colorscheme 'catppuccin'
 require('luasnip.loaders.from_lua').load { paths = '~/.config/nvim/LuaSnip/' }
 
+vim.cmd [[
+" Use Tab to expand and jump through snippets
+imap <silent><expr> <Tab> luasnip#expand_or_jumpable() ? '<Plug>luasnip-expand-or-jump' : '<Tab>' 
+smap <silent><expr> <Tab> luasnip#jumpable(1) ? '<Plug>luasnip-jump-next' : '<Tab>'
+
+" Use Shift-Tab to jump backwards through snippets
+imap <silent><expr> <S-Tab> luasnip#jumpable(-1) ? '<Plug>luasnip-jump-prev' : '<S-Tab>'
+smap <silent><expr> <S-Tab> luasnip#jumpable(-1) ? '<Plug>luasnip-jump-prev' : '<S-Tab>'
+]]
+require('luasnip.loaders.from_lua').load { paths = '~/.config/nvim/LuaSnip/' }
+require('luasnip').config.set_config { -- Setting LuaSnip config
+
+  -- Enable autotriggered snippets
+  enable_autosnippets = true,
+
+  -- Use Tab (or some other key if you prefer) to trigger visual selection
+  store_selection_keys = '<Tab>',
+}
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
